3393a62fd7c904a1b1950cf4bf3d134ebae534c7
sakots <sakots8000@gmail.com>
2020-07-16 22:12:32 +0900

    ver Up

--- theme_nee2/template_ini.php
+++ theme_nee2/template_ini.php
@@ -7,7 +7,7 @@
 */
 
 //テーマ(テンプレート)のバージョン
-define('TEMPLATE_VER', "v1.5.2 lot.200716");
+define('TEMPLATE_VER', "v1.5.3 lot.200716a");
 
 //エラーメッセージ
 define('MSG001', "該当記事がみつかりません[Log is not found.]");
