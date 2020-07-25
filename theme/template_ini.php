f4b43ef6403f34a8cded984d480ccced9a3c2fc8
sakots <sakots8000@gmail.com>
2020-07-25 22:15:43 +0900

    v1.8.1

--- theme/template_ini.php
+++ theme/template_ini.php
@@ -7,7 +7,7 @@
 */
 
 //テーマ(テンプレート)のバージョン
-define('TEMPLATE_VER', "v1.8.0 lot.200725");
+define('TEMPLATE_VER', "v1.8.1 lot.200725");
 
 //エラーメッセージ
 define('MSG001', "該当記事がみつかりません[Log is not found.]");
